22eabd59d2e04465626564d2a83ea3766f154466
lamloei <lamloei.com@gmail.com>
2018-05-23 18:35:33 +0700

    KidBright

---
+++ kidbright_i2c2/kidbright_i2c2.ino
@@ -0,0 +1,78 @@
+#include <Wire.h>
+TwoWire Wire2 = TwoWire(1);
+byte error, address;
+int nDevices;
+
+void setup()
+{
+  Wire.begin(21, 22);
+  Wire2.begin(4, 5);
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("\nI2C Scanner");
+}
+
+void loop()
+{
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("Wire: I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Wire: Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("Wire: No I2C devices found\n");
+  else
+    Serial.println("Wire: done\n");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    Wire2.beginTransmission(address);
+    error = Wire2.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("Wire2: I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Wire2: Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("Wire2: No I2C devices found\n");
+  else
+    Serial.println("Wire2: done\n");
+    
+  delay(2000);
+}
